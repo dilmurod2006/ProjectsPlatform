bf26735cea0ebeae09fae35aa2fb2d0c43481473
Bexruz Beveloper <texnoxack@gmail.com>
2024-07-30 03:12:15 +0500

    commit 2

--- KundalikCom/main.py
+++ KundalikCom/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from schemes import BuySerializer
+from database import *
+app = FastAPI()
+@app.post("/buy_api")
+async def root(data: BuySerializer):
 
+    return {"message": "Hello World"}
---
+++ KundalikCom/schemes.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+# Buy post api uchun serializer
+class BuySerializer(BaseModel):
+    token: str
+    months_count: int
