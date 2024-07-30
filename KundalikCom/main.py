15610e904650e28733bfd0181866954fb0832266
Bexruz Beveloper <texnoxack@gmail.com>
2024-07-30 05:45:24 +0500

    commit 3

---
+++ KundalikCom/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from schemes import BuySerializer
+from database import *
+
+app = FastAPI()
+@app.post("/ ")
+async def root(data: BuySerializer):
+
+    return {"message": "Hello World"}
