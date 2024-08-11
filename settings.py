0433acf90842cd5594143206ab82cd579f984606
Sizning Ismingiz <sizningemail@example.com>
2024-08-11 21:04:09 +0500

    remove uploads file
    remove

--- settings.py
+++ settings.py
@@ -30,8 +30,3 @@ PRODUCT_ID=os.environ.get('PRODUCT_ID')
 # ADMINS CONFUGRATIONS KEYS
 SECRET_KEY_FOR_ADMINS=os.environ.get('SECRET_KEY_FOR_ADMINS')
 
-
-
-# UPLOAD FILES
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'UPLOADFILES')
