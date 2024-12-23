0b515b37c42910fd6b6c4252e1631a05b1e6a7a5
Bexruz Boynazarov <texnoxack@gmail.com>
2024-12-22 23:52:46 -0800

    qidiruvchi yaxshilar

--- iqromindtest/iqromindtest.py
+++ iqromindtest/iqromindtest.py
@@ -618,13 +618,13 @@ async def get_all_natijalar(data: GetAllNatijalarSerializer, session: AsyncSessi
     if qmtest_user is None:
         raise HTTPException(status_code=401, detail="User mavjud emas!")
     # Mavjud bo'lsa
-    try:
+    if True:
         result = qmtest_user.testlar[data.month_date][data.test_key]["tekshirishlar"]
         # Sort qilish
         sorted_result = dict(sorted(result.items(), key=lambda x: string_to_number(x[1]), reverse=True))
 
         return sorted_result[:10]
-    except:
+    else:
         return []
 
 
