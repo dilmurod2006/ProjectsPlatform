aa7fbcd8d1ee1fe1e2601d1ea645c5cd5330a259
Dilmurod <dilmurodamonov006@gmail.com>
2025-02-17 15:14:13 +0500

    MongoDB API sozlandi
    MongoDB API  sozlandi

--- IqroMindTestUsers/iqromind_users.py
+++ IqroMindTestUsers/iqromind_users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 
 from models.models import User, Test
 from settings import IQROMIND_TEST_BOT_SECRET_KEY
-from database import users_collection
+from database import db1_collection
 from .schemas import get_test_serializer
 
 iqromind_users_router = APIRouter()
@@ -14,11 +14,11 @@ async def create_user(bot_secret_key: str, user: User):
         raise HTTPException(status_code=400, detail="Xato secret key!")
     
     # Agar tg_id bazada mavjud bo'lsa, foydalanuvchi ro'yxatdan o'tmasligi kerak
-    existing_user = await users_collection.find_one({"tg_id": user.tg_id})
+    existing_user = await db1_collection.find_one({"tg_id": user.tg_id})
     if existing_user is not None:
         raise HTTPException(status_code=400, detail="Bunaqa foydalanuvchi mavjud!")
     
-    await users_collection.insert_one(user.dict())
+    await db1_collection.insert_one(user.dict())
     return {"message": "Foydalanuvchi qo'shildi", "user_id": str(user.tg_id)}
 
 
@@ -28,10 +28,10 @@ async def create_user(bot_secret_key: str, user: User):
 @iqromind_users_router.post("/addtest/{user_id}")
 async def add_test(user_id: str, test: Test):
     # Foydalanuvchi mavjudligini tekshiramiz
-    if await users_collection.find_one({"user_id": user_id}):
+    if await db1_collection.find_one({"user_id": user_id}):
         # test obyektini dictionary ga aylantiramiz va kerak bo'lsa user_id ni yangilaymiz:
         test.user_id = user_id  # agar kerak bo'lsa
-        await users_collection.insert_one(test.dict())
+        await db1_collection.insert_one(test.dict())
         return {"message": "Test qo'shildi"}
     else:
         raise HTTPException(status_code=400, detail="Bunday foydalanuvchi mavjud emas!")
@@ -40,7 +40,7 @@ async def add_test(user_id: str, test: Test):
 # get tests by user_id
 @iqromind_users_router.get("/tests/{user_id}")
 async def get_tests(user_id: str):
-    cursor = users_collection.find({"user_id": user_id})
+    cursor = db1_collection.find({"user_id": user_id})
     tests = []
     async for test in cursor:
         tests.append(get_test_serializer(test))
--- database.py
+++ database.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 from settings import (
     DB_NAME,
@@ -12,6 +14,8 @@ from settings import (
     DB_HOST,
     DB_USER,
     DB_PASSWORD,
+    MONGODB_1_USERNAME,
+    MONGODB_1_PASSWORD
     )
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
@@ -24,12 +28,48 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
 
+
+
+# MONGODB setting ONLINE database1 
+
+
+uri = F"mongodb+srv://dilmurodamonov006:C2VJzXPDsvwn33Ef@iqromindtestbot.1kspd.mongodb.net/?retryWrites=true&w=majority&appName=IqroMindTestBot"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+database = client["IqroMindTestBot"]
+db1_collection = database["abuturent"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # MONGODB setting database
 # client = AsyncIOMotorClient(MONGO_DB_HOST)
-MONGO_URL = "mongodb://localhost:27017/abuturent"
-client = AsyncIOMotorClient(MONGO_URL)
-database = client["abuturent"]
+# MONGO_URL = "mongodb://localhost:27017/abuturent"
+# client = AsyncIOMotorClient(MONGO_URL)
+# database = client["abuturent"]
 
-users_collection = database["users"]
+# users_collection = database["users"]
 
 
