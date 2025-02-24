c470e441466c6f8c0c9a59578967b827da20ccd2
Bexruz Boynazarov <texnoxack@gmail.com>
2025-02-24 19:53:57 +0500

    iqro mind test: (update) get all antijalar

--- iqromindtest/utils.py
+++ iqromindtest/utils.py
@@ -66,8 +66,8 @@ def sort_dict(d: dict, page: int, ser: int):
     try:
         # Tartiblash: kattadan kichikka qarab
         for i in d.keys():
-            if ser != 0 and (ser == -1 or d[i][-3:] == "0.0") and (ser == 1 or d[i][-3:] != "0.0"):
-                del d[i]
+            if ser != 0 and (ser == -1 or d[i][1][-3:] == "0.0") and (ser == 1 or d[i][1][-3:] != "0.0"):
+                d.pop(i)
 
         sorted_items = sorted(d.items(), key=string_to_number, reverse=True)
         max_ball = -1
